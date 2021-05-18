79ded0aae8ef7400eb1d77645792c47e1ee6b57d
appleboblin <appleboblin2430@gmail.com>
2021-05-18 12:22:45 +0800

    Update twitter-hotfix.js
    Clean up

--- twitter-hotfix.js
+++ twitter-hotfix.js
@@ -1,45 +1,42 @@
+// requirements
 const twdl = require('twdl');
 const Discord = require('discord.js');
+const client = new Discord.Client();
+// require custom files
+const { prefix } = require('./config.json');
+const token = require('./token.json');
 const { command } = require('./function/generalCommands');
 const logger = require('./function/logger');
 
-const client = new Discord.Client();
-// require custom files
-const config = require('./config.json');
-const token = require('./token.json');
-
-const { prefix } = config;
-
+// arrays
 let tweetUrls = [
-  /* … */
+  // Individual tweets
   //'https://twitter.com/BrgrArt/status/1393359826693103616',
 ];
-
 let mediaUrls = [];
 
-async function downloadUrl(mediaUrl, tweetUrl, mediaData, options) {
-  mediaUrls.push(mediaUrl);
-  return mediaUrl;
-}
-
+// const
 const options = {
   downloadUrlFn: downloadUrl,
 };
 const dest = '835471079303544834';
 
-const nugget = async () => {
-  var results = await twdl.downloadUrls(tweetUrls, options);
+// functions
+const downloadUrl = async (mediaUrl, tweetUrl, mediaData, options) => {
+  mediaUrls.push(mediaUrl);
+  return mediaUrl;
+};
+const sendImage = async () => {
+  let results = await twdl.downloadUrls(tweetUrls, options);
   imgUrl = mediaUrls.forEach((e) => {
     //Send when getting info
     client.channels.cache.get(dest).send(e);
     logger.info('New image sent to channel');
-    var removeFirstInArray = mediaUrls.shift();
+    let removeFirstInArray = mediaUrls.shift();
   });
-  message.channel.send('hi');
   return;
 };
-
-function sendImage(client, triggerText) {
+const checkNewUrl = (client, triggerText) => {
   client.on('message', (message) => {
     if (
       //check where it receive the message from
@@ -49,22 +46,16 @@ function sendImage(client, triggerText) {
       // action
       tweetUrls = [];
       tweetUrls.push(message.content);
-      nugget();
+      sendImage();
       mediaUrls = [];
     }
   });
-}
+};
 
+// run when bot it online
 client.on('ready', () => {
-  sendImage(client);
+  checkNewUrl(client);
 });
-/*
-client.on('message', (message) => {
-  if (message.author.id === '199872338609569792') {
-    nugget();
-    return message.channel.send('stop');
-  }
-});*/
 
 // Login Discord
 client.login(token.discord_token);
