093b36ac277843bce52e6fc65406424434f4de5c
appleboblin <appleboblin2430@gmail.com>
2021-05-23 23:06:39 +0800

    Comments Added comments

--- commands/economy/lootbox.js
+++ commands/economy/lootbox.js
@@ -8,24 +8,37 @@ module.exports = {
   //minArgs: 0,
   maxArgs: 1,
   callback: (message, args) => {
+    //set empty array
     let items = [];
     let weight = [];
+    // check if matches
     if (args == 'normal') {
+      // load values
       let boxType = Type.BoxType.normal;
+      // get values of each item
       boxType.map((value, index) => {
+        // push into empty array
         items.push(value.item);
         weight.push(Number(value.weight));
       });
+      // get selected item
       result = roll.rollBox(weight);
+      // send item message
       return message.channel.send(`Item: ${items[result]}`);
     }
+    // check if matches
     if (args == 'rare') {
+      // load values
       let boxType = Type.BoxType.rare;
+      // get values of each item
       boxType.map((value, index) => {
+        // push into empty array
         items.push(value.item);
         weight.push(Number(value.weight));
       });
+      // get selected item
       result = roll.rollBox(weight);
+      // send item message
       return message.channel.send(`Item: ${items[result]}`);
     }
     message.channel.send(`Please choose either \`normal\` or \`rare\`.`);
