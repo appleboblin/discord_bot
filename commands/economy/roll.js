7da9ecc5a368e74e756f14185e0cdb3530e1ef7a
appleboblin <appleboblin2430@gmail.com>
2021-05-24 04:17:15 +0800

    Update roll.js
    Fix bug where can't receive capital  letters

--- commands/economy/roll.js
+++ commands/economy/roll.js
@@ -7,19 +7,18 @@ module.exports = {
   description: 'Roll Loot box',
   //minArgs: 0,
   maxArgs: 1,
-  callback: (message, chosenBox) => {
+  callback: async (message, chosenBox) => {
     //set empty array
     let items = [];
     let weight = [];
+    invalidBox = [];
     let result = 0;
     BoxList = Object.keys(Type.BoxType);
     BoxList.map((value) => {
-      invalidBox = [];
       invalidBox.push(value);
-
       if (
         // check if its the box selected is in list
-        value.toLowerCase().includes(chosenBox)
+        value.toString().toLowerCase() == chosenBox.toString().toLowerCase()
       ) {
         // true
         result += 1;
@@ -28,11 +27,10 @@ module.exports = {
         result += 0;
       }
     });
-    console.log(result);
     // if enough box match
     if (result == 1) {
       let boxType = Type.BoxType.normal;
-      // get values of each item
+      // get name each of boxes
       boxType.map((value, index) => {
         // push into empty array
         items.push(value.item);
@@ -47,7 +45,8 @@ module.exports = {
       // if not enough balance, prompt the user
     } else if (result == 0) {
       let warning = invalidBox.toString();
-      message.channel.send(`Please choose from ${warning}`);
+      warning = warning.replace(/[,]/g, ', ');
+      message.channel.send(`Please choose from ${warning}.`);
       // error handling
     } else {
       logger.error('failed');
