ddc7426802673a6a5cb599ae778a02810be713bb
appleboblin <appleboblin2430@gmail.com>
2021-05-24 03:59:20 +0800

    Update roll.js
    Update into dynamic instead of hard coded.

--- commands/economy/roll.js
+++ commands/economy/roll.js
@@ -7,13 +7,30 @@ module.exports = {
   description: 'Roll Loot box',
   //minArgs: 0,
   maxArgs: 1,
-  callback: (message, args) => {
+  callback: (message, chosenBox) => {
     //set empty array
     let items = [];
     let weight = [];
-    // check if matches
-    if (args == 'normal') {
-      // load values
+    let result = 0;
+    BoxList = Object.keys(Type.BoxType);
+    BoxList.map((value) => {
+      invalidBox = [];
+      invalidBox.push(value);
+
+      if (
+        // check if its the box selected is in list
+        value.toLowerCase().includes(chosenBox)
+      ) {
+        // true
+        result += 1;
+      } else {
+        // false
+        result += 0;
+      }
+    });
+    console.log(result);
+    // if enough box match
+    if (result == 1) {
       let boxType = Type.BoxType.normal;
       // get values of each item
       boxType.map((value, index) => {
@@ -27,41 +44,14 @@ module.exports = {
       itemsResult = items[result];
       trimmed = itemsResult.replace(/([A-Z])/g, ' $1').trim();
       return message.channel.send(`Item: ${trimmed}`);
+      // if not enough balance, prompt the user
+    } else if (result == 0) {
+      let warning = invalidBox.toString();
+      message.channel.send(`Please choose from ${warning}`);
+      // error handling
+    } else {
+      logger.error('failed');
+      message.channel.send('Something went wrong');
     }
-    // check if matches
-    if (args == 'rare') {
-      // load values
-      let boxType = Type.BoxType.rare;
-      // get values of each item
-      boxType.map((value, index) => {
-        // push into empty array
-        items.push(value.item);
-        weight.push(Number(value.weight));
-      });
-      // get selected item
-      result = roll.rollBox(weight);
-      // send item message
-      itemsResult = items[result];
-      trimmed = itemsResult.replace(/([A-Z])/g, ' $1').trim();
-      return message.channel.send(`Item: ${trimmed}`);
-    }
-    // check if matches
-    if (args == 'epic') {
-      // load values
-      let boxType = Type.BoxType.epic;
-      // get values of each item
-      boxType.map((value, index) => {
-        // push into empty array
-        items.push(value.item);
-        weight.push(Number(value.weight));
-      });
-      // get selected item
-      result = roll.rollBox(weight);
-      // send item message
-      itemsResult = items[result];
-      trimmed = itemsResult.replace(/([A-Z])/g, ' $1').trim();
-      return message.channel.send(`Item: ${trimmed}`);
-    }
-    message.channel.send(`Please choose either \`normal\` or \`rare\`.`);
   },
 };
