4cd3c6a8f044155fb6e9815aef3c986c6046906b
appleboblin <appleboblin2430@gmail.com>
2021-05-23 23:07:10 +0800

    Commants and logging Added comments and logging

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -25,53 +25,52 @@ module.exports = {
           url: 'https://cdn.discordapp.com/attachments/835471079303544834/845921169642618890/Screen_Shot_2021-05-23_at_3.06.38_PM.png',
         },
         fields: [],
+        footer: {
+          icon_url:
+            'https://cdn.discordapp.com/attachments/835471079303544834/846039122346377276/Screen_Shot_2021-05-23_at_10.57.00_PM.png',
+          text: 'placeholder',
+        },
       },
     };
-    if (shopItems.BoxType.length === 0)
-      return message.channel.send('No boxes available');
-    let boxList = Object.keys(shopItems.BoxType);
-    if (boxList.includes(args.toString())) {
-      const totalBox = await purchase.addBox(guildId, userId, args);
-      const inventory = totalBox.replace(',', '\n').replace(/[:]/g, ': ');
-      inventoryMenu.embed.fields.push({
-        name: `**Boxes**`,
-        value: `\`\`\`${inventory}\`\`\``,
-        inline: true,
-      });
 
-      message.channel.send(inventoryMenu);
-    } else {
-      message.channel.send('Invalid box type!');
-    }
-    /*
     const buyBox = async () => {
+      // check if BoxType is empty
       if (shopItems.BoxType.length === 0)
         return message.channel.send('No boxes available');
       let boxList = Object.keys(shopItems.BoxType);
+      //check if box type matches any in json
       if (boxList.includes(args.toString())) {
+        // increase box count
         const totalBox = await purchase.addBox(guildId, userId, args);
+        // take away coins needed
+        const pay = await economy.removeCoins(guildId, userId);
+        inventoryMenu.embed.footer.text = `Remaining Cock Coins: ${pay}`;
+        // prepare result for menu
         const inventory = totalBox.replace(',', '\n').replace(/[:]/g, ': ');
+        // send info to menu
         inventoryMenu.embed.fields.push({
           name: `**Boxes**`,
           value: `\`\`\`${inventory}\`\`\``,
           inline: true,
         });
-
+        // send menu to channel
         message.channel.send(inventoryMenu);
       } else {
         message.channel.send('Invalid box type!');
       }
     };
-    //let hi = await purchase.getBox(guildId, userId, args);
-    //console.log(hi);
+    // check if user have enough balance
     const check = await economy.checkBalance(guildId, userId, args);
+    // if enough balance, buy
     if (check == 1) {
-      console.log('enough');
       buyBox();
+      // if not enough balance, prompt the user
     } else if (check == 0) {
-      console.log('not enough');
+      message.channel.send(`You don't have enough Cock Coins!`);
+      // error handling
     } else {
-      console.log('failed');
-    }*/
+      logger.error('failed');
+      message.send('Something went wrong');
+    }
   },
 };
