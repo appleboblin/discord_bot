647ec8a259d3a7382c89b412b9ec73bb3187ece5
appleboblin <appleboblin2430@gmail.com>
2021-05-24 13:57:22 +0800

    Update ding.js Update formatting, changed to array instead of single string. Added options to help.js

--- commands/commands/help.js
+++ commands/commands/help.js
@@ -5,6 +5,12 @@ const logger = require('../../util/logger');
 module.exports = {
   commands: ['help', 'h'],
   description: 'Lists all commands',
+  expectedArgs: '<media>/<list>',
+  minArgs: 1,
+  maxArgs: 1,
+  //requiredRoles: [`test`],
+  //permissions: ['SEND_MESSAGES'],
+  //permissionError = 'You do not have permission to run this command.',
   callback: (message) => {
     let reply = {
       embed: {
